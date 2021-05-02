655c0157c331165a7ca1be5f76af2db204f7a59b
tron66 <tusiime.ronald@gmail.com>
2021-05-02 15:59:13 +0100

    First initial commit

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+         'id': 1,
+         'title': 'ARTICLE ONE',
+         'body': 'This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.',
+         'author': 'Theo Cleavoyent',
+         'current_date': '07-12-2020'   
+        },
+        {
+         'id': 2,
+         'title': 'ARTICLE TWO',
+         'body': 'This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.',
+         'author': 'Augustus Clean',
+         'current_date': '14-04-2020'   
+        },
+        {
+         'id': 3,
+         'title': 'ARTICLE THREE',
+         'body': 'This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.',
+         'author': 'MAry Skills',
+         'current_date': '30-10-2020'               
+        }
+    ]
+    return articles
